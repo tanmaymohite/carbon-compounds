4355fa0f7e6b9c4844a33d561286ecff806b785a
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-08-04 23:07:30 +0530

    arrays

---
+++ functionArray.py
@@ -0,0 +1,13 @@
+def my_function (r):
+    print('The worlds tallest building is ' + (r) )
+
+my_function( r ='Burj khalifa')
+
+c = ['lamborghini','tesla','buggati','pagani','porche']
+c.append('tata Evision')
+c.remove('buggati')
+c.sort()
+c.pop(0)
+print('\n')
+for a in c:
+    print(a)
