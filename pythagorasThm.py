c9246bab3019c26def3339c1db72054795845b35
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-08-15 12:31:20 +0530

    g

---
+++ pythagorasThm.py
@@ -0,0 +1,21 @@
+# program on geometric mean
+
+from math import sqrt
+
+a = input('Enter the value of hypotenuse:')
+a = int(a)
+
+b = ('Enter the value of height:')
+b = int (b)
+
+c = ('Enter the value of base')
+c = int (c)
+
+
+
+Hypotenuse = b**2 + c**2
+
+hypotenuse = sqrt(hypotenuse)
+
+
+
